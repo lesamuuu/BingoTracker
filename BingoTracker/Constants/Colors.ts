3f2a66fe3b41bdb73347cda8975f096ba56b2d2f
Samuru <samuelpayback@gmail.com>
2024-04-23 17:56:27 +0100

    Constants

---
+++ BingoTracker/Constants/Colors.ts
@@ -0,0 +1,6 @@
+const COLORS = {
+    defaultBackground: '#ffffff',
+    defaultBall: '#5a8bed'
+}
+
+export default COLORS;
--- BingoTracker/Constants/Strings.ts
+++ BingoTracker/Constants/Strings.ts
@@ -2,6 +2,9 @@ const STRINGS = {
     Generics: {
         Cancel: 'Cancelar',
     },
+    HomeScreen: {
+        NoBalls: 'No hay bolas en el tablero. Para añadir una, haz click en el icono: ',
+    },
     NewBallModal: {
         NumberAlreadyExists: 'Este número ya existe, no se puede volver a introducir',
         AddBall: 'Añadir Bola'
@@ -10,8 +13,24 @@ const STRINGS = {
         BallSize: 'Tamaño de las bolas',
         AddBall: 'Añadir Bola',
 
-        BallColor: 'Color de las Bolas'
+        BallColor: 'Color de las Bolas',
+        NumberColor: 'Color del número',
+    },
+    Alerts: {
+        Generic: {
+            Cancel: 'Cancelar',
+            Delete: 'Eliminar',
+        },
+        Reset: {
+            Title: 'Resetear el tablero?',
+            Text: 'Seguro que quieres resetear el tablero? Esto eliminará todas las bolas'
+        },
+        DeleteBall: {
+            Title: 'Eliminar bola?',
+            Text: 'Seguro que quieres eliminar esta bola?'
+        }
     }
 }
+
 
 export default STRINGS;
