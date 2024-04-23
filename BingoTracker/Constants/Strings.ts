7e54291f5290625d0e2d384d01ac1d8f3adefea0
Samuru <samuelpayback@gmail.com>
2024-04-23 15:11:54 +0100

    Constants (Spanish strings)

---
+++ BingoTracker/Constants/Strings.ts
@@ -0,0 +1,17 @@
+const STRINGS = {
+    Generics: {
+        Cancel: 'Cancelar',
+    },
+    NewBallModal: {
+        NumberAlreadyExists: 'Este número ya existe, no se puede volver a introducir',
+        AddBall: 'Añadir Bola'
+    },
+    SettingsModal: {
+        BallSize: 'Tamaño de las bolas',
+        AddBall: 'Añadir Bola',
+
+        BallColor: 'Color de las Bolas'
+    }
+}
+
+export default STRINGS;
