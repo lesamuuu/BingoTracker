e68f2edb74ddf32c24110d5776026c959bc26cef
Samuru <samuelpayback@gmail.com>
2024-04-24 17:19:45 +0100

    Constants

--- BingoTracker/Constants/Colors.ts
+++ BingoTracker/Constants/Colors.ts
@@ -2,7 +2,11 @@ const COLORS = {
     defaultBackground: '#ffffff',
     defaultBall: '#5a8bed',
     currentCall: '#3ab327',
-    totalCalls: '#ded447'
+    totalCalls: '#ded447',
+    ColorSelectionUnSelected: '#eeeeee',
+    ColorSelectionSelected: 'lightblue',
+    ModalButton: '#2196F3',
+    Shadow: '#000000',
 }
 
 export default COLORS;
--- BingoTracker/Constants/Sizes.ts
+++ BingoTracker/Constants/Sizes.ts
@@ -6,6 +6,12 @@ const SIZES = {
 
     RecentBallTitlePhone: 12,
     RecentBallTitleTablet: 25,
+
+    SettingsSectionTitlePhone: 16,
+    SettingsSectionTitleTablet: 22,
+
+    SettingsTextPhone: 12,
+    SettingsTextTablet: 18,
 }
 
 export default SIZES;
